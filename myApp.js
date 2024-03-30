b51e63a1a2282a6f291465d6c457be7792c6d0a7
Alek Lucatero <lucaalek99@gmail.com>
2024-03-30 11:33:17 -0700

    Responded to request a get request with a file
    I responded to a get request with a file using res.sendFile(__dirname + file)

--- myApp.js
+++ myApp.js
@@ -1,13 +1,16 @@
 let express = require('express');
 let app = express();
 
-//Serving a string
-app.get("/", (req, res) => {
-    res.send("Hello Express");
-});
+// //Serving a string
+// app.get("/", (req, res) => {
+//     res.send("Hello Express");
+// });
 
-
-
+// Responding to requests with a file using res.sendFile(path)
+//In this excercise we are serving and index.html file on "/"
+app.get(`/`, (req, res) => {
+    res.sendFile(__dirname + `/views/index.html`)
+})
 
 
 
